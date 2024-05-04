4cbf1d4761a8f49baae2c4e1f4704e494844241a
Caio977 <168917067+Caio977@users.noreply.github.com>
2024-05-04 11:50:25 -0300

    Opção de sair
    Opção de sair do sistema no menu

--- ProjetoModulo.cpp
+++ ProjetoModulo.cpp
@@ -117,7 +117,8 @@ int main()
         printf("\tEscolha a opção desejada no menu:\n\n");
         printf("\t1 Registrar nomes\n");
         printf("\t2 Consultar nomes\n");
-        printf("\t3 Deletar nomes\n\n");
+        printf("\t3 Deletar nomes\n");
+        printf("\t4 Sair do sistema\n\n");
         printf("Opção: "); // escolha da opçao e fim do menu
         
 		scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -136,6 +137,11 @@ int main()
 		
 		case 3:	
 		deletar();
+		break;
+		
+		case 4:
+		printf("Obrigado por utilizar o sistema!");	
+		return 0;
 		break;
 		
 		default:
